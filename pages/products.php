baaba3724962f504ba1d6cf5d7da021934af49d4
Stephan Rübner <stephan@ruebner.me>
2018-07-04 21:39:15 +0200

    Reorganized application and provided basic html layout for the example shop page.

---
+++ pages/products.php
@@ -0,0 +1,8 @@
+<?php
+
+?>
+
+<form method="post" action="index.php?page=products">
+    <input type="text" name="search_query">
+    <button type="submit" name="search_submit">Search Products</button>
+</form>
