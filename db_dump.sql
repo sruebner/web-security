7b9caf55c471afdf34ffc99fc3fc171d3423820b
Stephan Rübner <stephan@ruebner.me>
2018-07-23 09:52:58 +0200

    Removed comments from sql dump.

--- db_dump.sql
+++ db_dump.sql
@@ -1,24 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 4.7.7
--- https://www.phpmyadmin.net/
---
--- Host: localhost:8889
--- Erstellungszeit: 23. Jul 2018 um 07:23
--- Server-Version: 5.6.38
--- PHP-Version: 7.2.1
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
---
--- Datenbank: `webshop`
---
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `guestbook`
---
 
 CREATE TABLE `guestbook` (
   `id` int(11) NOT NULL,
@@ -27,18 +8,8 @@ CREATE TABLE `guestbook` (
   `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Daten für Tabelle `guestbook`
---
-
 INSERT INTO `guestbook` (`id`, `name`, `text`, `date`) VALUES
   (1, 'Steve', 'Tolle Webseite, klasse Produkte, gerne wieder! *****', '2018-07-21 12:58:10');
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `products`
---
 
 CREATE TABLE `products` (
   `id` int(11) NOT NULL,
@@ -46,10 +17,6 @@ CREATE TABLE `products` (
   `description` text,
   `price` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Daten für Tabelle `products`
---
 
 INSERT INTO `products` (`id`, `name`, `description`, `price`) VALUES
   (1, 'Tisch SJÄLLAND', 'Die Tischplatte besteht aus Eukalyptusholz und sorgt für aromatische Entspannung bei Frühstück, Mittag- und Abendessen.', 17900),
@@ -59,23 +26,11 @@ INSERT INTO `products` (`id`, `name`, `description`, `price`) VALUES
   (5, 'Regal KALLAX', 'Flexibel, quadratisch, praktisch, gut! Das Kallax Regal lässt sich stellen oder legen und passt so gut in jeden Raum.', 5900),
   (6, 'Regal BILLY', 'Billy ist der ewig moderne Favorit unter den Regalen.', 5900);
 
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `sessions`
---
-
 CREATE TABLE `sessions` (
   `id` varchar(128) NOT NULL,
   `data` text NOT NULL,
   `creation_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `users`
---
 
 CREATE TABLE `users` (
   `username` varchar(128) NOT NULL DEFAULT '',
@@ -83,55 +38,27 @@ CREATE TABLE `users` (
   `is_admin` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Daten für Tabelle `users`
---
-
 INSERT INTO `users` (`username`, `password`, `is_admin`) VALUES
   ('admin', '$2y$10$MlMCmvDpeujjiFVjxqmw3uG4xSACUqesbooyHjaotnCkvWi0.Ribm', 1),
   ('Judy', '$2y$10$qdC9LroYzH0omCVtBmPZquq3AiCZlCYXRdAMRPmAThBLxJTkrSVK.', 0),
   ('Max', '$2y$10$mRCNrmcF/vyWQql6UIdHGuApolIQOhqNjMA6.EPtCI5IZqXndJ8na', 0);
 
---
--- Indizes der exportierten Tabellen
---
 
---
--- Indizes für die Tabelle `guestbook`
---
 ALTER TABLE `guestbook`
   ADD PRIMARY KEY (`id`);
 
---
--- Indizes für die Tabelle `products`
---
 ALTER TABLE `products`
   ADD PRIMARY KEY (`id`);
 
---
--- Indizes für die Tabelle `sessions`
---
 ALTER TABLE `sessions`
   ADD PRIMARY KEY (`id`);
 
---
--- Indizes für die Tabelle `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`username`);
 
---
--- AUTO_INCREMENT für exportierte Tabellen
---
 
---
--- AUTO_INCREMENT für Tabelle `guestbook`
---
 ALTER TABLE `guestbook`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- AUTO_INCREMENT für Tabelle `products`
---
 ALTER TABLE `products`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
